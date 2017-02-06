208e09ae064b10a56e15605958c24451922d5d70
Benjamin <bsmith@smithlite.com>
2017-02-06 15:02:43 -0800

    working single post bot

--- bot.py
+++ bot.py
@@ -1,24 +1,42 @@
+#!/usr/bin/env python
+
 import os
 import tweepy
+import ConfigParser
+import inspect
+import hashlib
+from tweepy import Stream
+from tweepy import StreamListener
+from tweepy import OAuthHandler
 from secrets import *
 from time import gmtime, strftime
 
-# individual bot config
+# Individual bot config
+# Replace with your bot name
 bot_username = 'codingbot1000'
 logfile_name = bot_username + ".log"
 
-#
+auth = OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 
+#class TweetListener(StreamListener):
+#	def on_status(self, status):
+#		print "tweet" + str(status.created_at) + "\n"
+#		print status.ext + "\n"
+#stream = Stream(auth, TweetListener(), secure=True, )
+#t = u"#code" + u"coding" + u"#programming"
+#stream.filter(track=[t])
+
+#
 def create_tweet():
 	"""Crease the text of the tweet you want to send"""
 	#replace with with custom code
-	text = ""
+	text = "My first tweet"
 	return text
 
 def tweet(text): 
 	"""Send out the text as a tweet"""
-	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+	
 	api = tweepy.API(auth)
 
 
@@ -37,7 +55,7 @@ def log(message):
 	path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 	with open(os.path.join(path, logfile_name), 'a+') as f:
 		t = strftime("%d %b %Y %H:%M:%S", gmtime())
-		f.write("\n" + t + " " + message)
+		f.write("\n" + t + " " + str(message))
 
 
 if __name__ == "__main__":
