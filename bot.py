5fdfe3ed0396d37c221bdefa6f2b72cfc8ccf29b
Benjamin <bsmith@smithlite.com>
2017-02-06 22:50:46 -0800

    retweeting functionality compelte and included comments

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# import libraries
 import os
 import tweepy
 import json
@@ -20,14 +21,19 @@ warnings.filterwarnings("ignore")
 bot_username = 'codingbot1000'
 logfile_name = bot_username + ".log"
 
+# Autheticating keys
 auth_handler = OAuthHandler(C_KEY, C_SECRET)
 auth_handler.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 
+# Autheticating client
 twitter_client = API(auth_handler)
 
+# set logging
 logging.getLogger("main").setLevel(logging.INFO)
 
+# exclude these keywords
 AVOID = ["java"]
+
 
 class PyStreamListener(StreamListener):
 	def on_data(self, data):
@@ -35,25 +41,44 @@ class PyStreamListener(StreamListener):
 		try:
 			try:
 				publish = True
+
+				#if tweet includes excluded words don't retweet
 				for word in AVOID:
 					if word in tweet['text'].lower():
 						logging.info("SKIPPED FOR {}".format(word))
 						publish = False
+
+				# if tweet is not in english don't retweet
 				if tweet.get('lang') and tweet.get('lang') != 'en':
 					publish = False
+
+				# if tweet hasnt been retweeted
 				if publish:
+					# if new id - retweet
 					twitter_client.retweet(tweet['id_str'])
 					logging.debug("RT: {}".format(tweet['text']))
 					log("Retweeted: " + tweet['id_str'])
+					#print twitter_client.rate_limit_status()
+			# exception handling for failed retweeting		
 			except Exception as e:
 				logging.error(e)
 				log(e.message)
+
+				# ugly logging of rate limit status
 				#log(twitter_client.rate_limit_status())
 			return True
-		except tweepy.RateLimitError:
+
+		# exception handling for rate limits	
+		except TweepError:
 			handle_rate_limit_error()
+			log("sleeping")
+			time.sleep(60*15)
 
 	def on_error(self, status):
+		if status == 420:
+			# disconnect stream if rate limit is reached
+			log("Disconnecting stream")
+			return False
 		print status
 
 
@@ -79,7 +104,7 @@ class PyStreamListener(StreamListener):
 #		log("Tweeted: " + text)
 
 
-
+# alternate use for message logging
 def log(message):
 	"""Log message to logfile"""
 	path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -88,9 +113,9 @@ def log(message):
 		f.write("\n" + t + " " + str(message))
 
 
+# main execution
 if __name__ == "__main__":
-#	tweet_text = create_tweet()
-#	tweet(tweet_text)
 	listener = PyStreamListener()
 	stream = Stream(auth_handler, listener)
-	stream.filter(track=['#develop', '#coding', '#programming'])
+	# which hashtags to track and send to stream
+	stream.filter(track=['#develop', '#coding', '#programming', '#computerscience', '#compsci', '#softwaredeveloper', '#software'])
