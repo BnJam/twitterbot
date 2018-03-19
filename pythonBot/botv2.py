4a9a0bea0701ea4082081320586858c2c7f57dbb
benjamin <benjamin.smith122490@gmail.com>
2018-03-18 21:16:05 -0700

    bot version 2 and accompanying csv

--- pythonBot/botv2.py
+++ pythonBot/botv2.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 # Author: Benjamin Smith
-# Date:   6th Feb 2017
-# File: bot.py
-# Purpose: Retweet tweets associated toward computer science topics
+# Date:   March 14 2018
+# File: botv2.py
+# Purpose: Retweet tweets associated with Python
 
 # import libraries
 import os
 import tweepy 
 import json
-import logging
+import csv
 import warnings
 import time
 import http.client
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 
 # Individual bot config
 # Replace with your bot name
-bot_username = 'codingbot1000'
+bot_username = '@codingbot1000'
 logfile_name = bot_username + ".log"
 
 # Autheticating keys
@@ -29,30 +29,46 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+# open csv for data collection
+header = ["User_ID", "Username", "Date", "Tweet_ID"]
 
-# set logging
-logging.getLogger("main").setLevel(logging.INFO)
 
 # exclude these keywords
 AVOID = ["java" ]
 
-hashtags = ['#python','#developer','#py']
+q = ['#python','#developer','#py']
 
 class pyBot:
 	print("pyBotv2")
+
 	def re_tweet(self):
 		try:
-			for tweet in tweepy.Cursor(api.search,q='#python').items(200):
-				print("Retweet by: @" + tweet.user.screen_name)
-				tweet.retweet()
-				print("Retweeted: " + tweet.id_str)
-				tweet.favorite()
-				print("Sleeping for 5 mins...")
-				time.sleep(5*60)
+			with open("botv2.csv", 'a') as datalog:
+				writer = csv.writer(datalog, delimiter=',')
+				for tweet in tweepy.Cursor(api.search,q='#python', lang='en').items(1000):
+					if(tweet.user.screen_name == bot_username):
+						pass
+					else:
+						ht = []
+						for hashtag in tweet.entities['hashtags']:
+							ht.append(hashtag['text'])
+						if(tweet.retweet()):
+							writer.writerow([tweet.user.id_str, tweet.user.screen_name, tweet.created_at, tweet.id_str, ht])
+							print("Retweet by: @" + tweet.user.screen_name)
+							print("Retweeted: " + tweet.id_str)
+							tweet.favorite()
+							print("Favorited: " + tweet.id_str)
+							print("Sleeping for 2 mins...")
+							time.sleep(2*60)
+						else:
+							print("Already retweeted...")
+							continue
 		except tweepy.TweepError as e:
-			print(e.reason)
+			print("Error Thrown: "+ e.reason)
 		except StopIteration:
-			return
+			print("StopIteration")
+
+
 
 if __name__ == "__main__":
 	pybot = pyBot()
